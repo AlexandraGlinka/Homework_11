fd9a6b952dccf83842a1185962d7f4ec381ab3d7
glinka <alexandraglinka@yandex.ru>
2023-03-19 23:57:26 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void printYearType(int yearCheck) {
+        if ((yearCheck % 4 == 0 && yearCheck % 100 != 0) || yearCheck % 400 == 0) {
+            System.out.println(yearCheck + " год является високосным");
+        } else {
+            System.out.println(yearCheck + " год не является високосным");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+//        Реализуйте метод, который получает в качестве параметра год, проверяет,
+//        является ли он високосным, и выводит результат в консоль.
+//        високосным является каждый четвертый год, но не является каждый сотый.
+//        Также високосным является каждый четырехсотый год.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите год - ");
+        int year = sc.nextInt();
+        printYearType(year);
     }
 }
