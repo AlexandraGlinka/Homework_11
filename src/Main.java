5f5e52480a0a7ae1a245d5966a29bd45705aa946
glinka <alexandraglinka@yandex.ru>
2023-03-20 01:04:13 +0300

    Task_2 HELP SOS

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +9,47 @@ public class Main {
             System.out.println(yearCheck + " год не является високосным");
         }
     }
+
+    public static void printClientVersion(String nameOs, int yearOfCreation) { // проверяем ОС и год для задачи 2
+        int os;
+        int currentYear = LocalDate.now().getYear(); // текущий год
+
+        if (nameOs.equals("iOS")) {
+            os = 0; // iOs
+        } else {
+            os = 1; // Andriod
+        }
+
+        // !!!!!!!!!!!!!!
+        // ПОЧЕМУ ЕСЛИ Я ДОБАВЛЯЮ УСЛОВИЕ ELSE IF, ВЫСВЕЛИЧВАЕТ ОШИБКУ, ЧТО НАДО ИНИЦИАЛИЗИРОВАТЬ OS ???
+        // если использовать закомментированный кусок ниже, а не сверху
+        // хотела дополнительно сделать проверку на регистр. не понимаю почему ошибка
+//        if (nameOs.toLowerCase().equals("ios")) {
+//            os = 0; // iOs
+//        } else if (nameOs.toLowerCase().equals("andriod")) {
+//            os = 1; // Andriod
+//        }
+
+        if (os == 0) {
+            if (yearOfCreation < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (os == 1) {
+            if (yearOfCreation < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Andrion по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -23,4 +63,18 @@ public class Main {
         int year = sc.nextInt();
         printYearType(year);
     }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите операционную систему - ");
+        String osName = sc.next(); // вводим модель
+
+        System.out.print("Введите год выпуска телефона - ");
+        int clientDeviceYear = sc.nextInt(); // вводим год выпуска
+
+        printClientVersion(osName, clientDeviceYear); // идем в метод и делаем проверку
+    }
+
 }
